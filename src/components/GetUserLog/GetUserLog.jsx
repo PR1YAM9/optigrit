24661b527fee0657f36c94cce3e2ae2c5caa7283
Priyam Maini <priyam9maini@gmail.com>
2023-11-03 18:01:29 +0530

    Update GetUserLog component styling and functionality

--- src/components/GetUserLog/GetUserLog.jsx
+++ src/components/GetUserLog/GetUserLog.jsx
@@ -5,22 +5,24 @@ import './GetUserLog.css';
 const GetUserLog = () => {
   const [searchField, setSearchField] = useState('');
   const [searchValue, setSearchValue] = useState('');
-  const [showFullContent, setShowFullContent] = useState({});
   const [visibleFields, setVisibleFields] = useState(Object.keys(data[0]));
   const [logs, setLogs] = useState(null);
+  const [showFullContent, setShowFullContent] = useState(data.map(() => false));
 
-  const toggleShowFullContent = (field) => {
-    setShowFullContent((prev) => ({
-      ...prev,
-      [field]: !prev[field],
-    }));
+  const toggleShowFullContent = (index) => {
+    setShowFullContent((prev) => {
+      const updatedShowFullContent = [...prev];
+      updatedShowFullContent[index] = !updatedShowFullContent[index];
+      return updatedShowFullContent;
+    });
   };
 
-  const toggleShowLessContent = (field) => {
-    setShowFullContent((prev) => ({
-      ...prev,
-      [field]: false,
-    }));
+  const toggleShowLessContent = (index) => {
+    setShowFullContent((prev) => {
+      const updatedShowFullContent = [...prev];
+      updatedShowFullContent[index] = false;
+      return updatedShowFullContent;
+    });
   };
 
   const handleFieldVisibilityChange = (field) => {
@@ -44,33 +46,32 @@ const GetUserLog = () => {
 
     setLogs(results);
   };
-  
 
   return (
     <div className="cover">
-    <div className="topCover">
-      <h1 className="heading">Get User Log</h1>
-      <div className="searchFileda">
-        <select
-          className="select"
-          value={searchField}
-          onChange={(e) => setSearchField(e.target.value)}
-        >
-          <option value="">Select a field to search</option>
-          {Object.keys(data[0]).map((field) => (
-            <option key={field} value={field}>
-              {field}
-            </option>
-          ))}
-        </select>
-        <input
-          className="select"
-          type="text"
-          placeholder={`Search by ${searchField}...`}
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-        />
-      </div>
+      <div className="topCover">
+        <h1 className="heading">Get User Log</h1>
+        <div className="searchFileda">
+          <select
+            className="select"
+            value={searchField}
+            onChange={(e) => setSearchField(e.target.value)}
+          >
+            <option value="">Select a field to search</option>
+            {Object.keys(data[0]).map((field) => (
+              <option key={field} value={field}>
+                {field}
+              </option>
+            ))}
+          </select>
+          <input
+            className="select"
+            type="text"
+            placeholder={`Search by ${searchField}...`}
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </div>
       </div>
       <div className="section2">
         <h2 className="VF">Visible Fields</h2>
@@ -87,9 +88,9 @@ const GetUserLog = () => {
             </button>
           ))}
         </div>
-      <button className="submit-button" onClick={handleDataSubmit}>
-        GET LOGS
-      </button>
+        <button className="submit-button" onClick={handleDataSubmit}>
+          GET LOGS
+        </button>
       </div>
       {logs && logs.length > 0 && (
         <div className="data">
@@ -98,33 +99,29 @@ const GetUserLog = () => {
               <tr>
                 {visibleFields.map((field) => (
                   <th className={`th${field}`} key={field}>
-                    {field}
+                    {field.toUpperCase()}
                   </th>
                 ))}
               </tr>
             </thead>
             <tbody>
-              {logs.map((log) => (
+              {logs.map((log, index) => (
                 <tr className="log" key={log.log_id}>
                   {visibleFields.map((field) => (
                     <td key={field} className={field}>
                       {log[field].length > 20 ? (
                         <div>
-                          {showFullContent[field] ? (
+                          {showFullContent[index] ? (
                             <>
                               {log[field]}{' '}
-                              <button className='show'
-                                onClick={() => toggleShowLessContent(field)}
-                              >
+                              <button className='show' onClick={() => toggleShowLessContent(index)}>
                                 Show less
                               </button>
                             </>
                           ) : (
                             <>
                               {log[field].slice(0, 20)}{' '}
-                              <button className='show'
-                                onClick={() => toggleShowFullContent(field)}
-                              >
+                              <button className='show' onClick={() => toggleShowFullContent(index)}>
                                 Show more
                               </button>
                             </>
