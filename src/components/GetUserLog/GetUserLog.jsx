5d84eb46cdbe869f85b5bb04a6e3c9e80f414bac
Priyam Maini <priyam9maini@gmail.com>
2023-11-08 00:42:38 +0530

    Add multiple search fields and values to GetUserLog component

--- src/components/GetUserLog/GetUserLog.jsx
+++ src/components/GetUserLog/GetUserLog.jsx
@@ -3,8 +3,8 @@ import data from '../../MOCK_DATA.json';
 import './GetUserLog.css';
 
 const GetUserLog = () => {
-  const [searchField, setSearchField] = useState('');
-  const [searchValue, setSearchValue] = useState('');
+  const [searchFields, setSearchFields] = useState(['']); // Initialize with an empty field
+  const [searchValues, setSearchValues] = useState(['']); // Initialize with an empty value
   const [visibleFields, setVisibleFields] = useState(Object.keys(data[0]));
   const [logs, setLogs] = useState(null);
   const [showFullContent, setShowFullContent] = useState(data.map(() => false));
@@ -33,15 +33,23 @@ const GetUserLog = () => {
     }
   };
 
+  const handleFieldSelectionChange = () => {
+    setSearchFields([...searchFields, '']);
+    setSearchValues([...searchValues, '']);
+  };
+
   const handleDataSubmit = () => {
-    if (!searchField || !searchValue) {
+    if (searchFields.length === 0 || searchValues.some((value) => value === '')) {
       setLogs([]);
       return;
     }
 
     const results = data.filter((log) => {
-      const fieldValue = log[searchField];
-      return fieldValue.toString().toLowerCase().includes(searchValue.toLowerCase());
+      return searchFields.some((field, index) => {
+        const fieldValue = log[field];
+        const searchValue = searchValues[index];
+        return fieldValue.toString().toLowerCase().includes(searchValue.toLowerCase());
+      });
     });
 
     setLogs(results);
@@ -51,26 +59,44 @@ const GetUserLog = () => {
     <div className="cover">
       <div className="topCover">
         <h1 className="heading">Get User Log</h1>
-        <div className="searchFileda">
-          <select
-            className="select"
-            value={searchField}
-            onChange={(e) => setSearchField(e.target.value)}
-          >
-            <option value="">Select a field to search</option>
-            {Object.keys(data[0]).map((field) => (
-              <option key={field} value={field}>
-                {field}
-              </option>
-            ))}
-          </select>
-          <input
-            className="select"
-            type="text"
-            placeholder={`Search by ${searchField}...`}
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-          />
+        <div className="searchFields">
+          {searchFields.map((field, index) => (
+            <div className='fields' key={index}>
+              <select
+                className="select"
+                value={field}
+                onChange={(e) => {
+                  const updatedFields = [...searchFields];
+                  updatedFields[index] = e.target.value;
+                  setSearchFields(updatedFields);
+                }}
+              >
+                <option value="">Select a field to search</option>
+                {Object.keys(data[0]).map((f) => (
+                  <option key={f} value={f}>
+                    {f}
+                  </option>
+                ))}
+              </select>
+              <input
+                className="select"
+                type="text"
+                placeholder={`Search by ${field}...`}
+                value={searchValues[index]}
+                onChange={(e) => {
+                  const updatedValues = [...searchValues];
+                  updatedValues[index] = e.target.value;
+                  setSearchValues(updatedValues);
+                }}
+              />
+            </div>
+          ))}
+          <button className="submit-button" onClick={handleFieldSelectionChange}>
+            + Add Field
+          </button>
+        <button className="submit-button" onClick={handleDataSubmit}>
+          GET LOGS
+        </button>
         </div>
       </div>
       <div className="section2">
@@ -88,9 +114,6 @@ const GetUserLog = () => {
             </button>
           ))}
         </div>
-        <button className="submit-button" onClick={handleDataSubmit}>
-          GET LOGS
-        </button>
       </div>
       {logs && logs.length > 0 && (
         <div className="data">
